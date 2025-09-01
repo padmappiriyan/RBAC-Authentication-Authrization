6a7a1b3cc07d6bffaac9d0abda8e8b5e69000c9c
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-01 21:26:59 +0530

    Create the RBAC

--- src/index.js
+++ src/index.js
@@ -7,3 +7,10 @@ const app= express();
 
 //Built-in Middleware 
 app.use(express.json());
+
+//Start the Server
+const PORT = process.env.PORT || 7002;
+
+app.listen(PORT,()=>{
+    console.log(`port is running in ${PORT}`);
+})
