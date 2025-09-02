349f89edf8e2298efea1067c60369af91fe39396
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-02 12:50:52 +0530

    Update the roleMiddleware

--- src/middlewares/roleMiddleware.js
+++ src/middlewares/roleMiddleware.js
@@ -1,7 +1,7 @@
 const authorizeRoles = (...allowedRoles) => {
     
     return (req, res, next) => {
-        console.log(req.user.role);
+        
         if (!allowedRoles.includes(req.user.role)) {
            
             return res.status(403).json({
