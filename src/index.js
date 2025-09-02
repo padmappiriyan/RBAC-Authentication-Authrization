ebc33881d95831c2f78c6d5b3dec050cd19914eb
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-02 11:48:43 +0530

    main file index.js file has updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import dbConnect from "./config/dbConnect.js";
 import authRoutes from "./routes/authRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -11,7 +13,11 @@ dbConnect();
 
 //Built-in Middleware 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
+app.use("/api/user",userRoutes);
+
 
 //Start the Server
 const PORT = process.env.PORT || 7002;
