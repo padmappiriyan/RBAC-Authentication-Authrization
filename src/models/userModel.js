b9091f960e7d7b2bfe9dbb445f570b223986d56b
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-09-02 11:47:33 +0530

    Update UserModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,7 +14,7 @@ const userSchema= new mongoose.Schema({
     },
     role:{
         type:String,
-        enum:["admin","user","manager"]
+        enum:["Admin","User","Manager"]
     }
 },{
     timestamps:true
